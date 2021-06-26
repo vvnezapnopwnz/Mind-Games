8fab3af70443a15f3bdfe3f129f58871fa4648e4
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-06-26 14:38:23 +0600

    stage 5-2

--- src/belogic.js
+++ src/belogic.js
@@ -1,26 +1,24 @@
 import readlineSync from 'readline-sync';
-import { welcomeUser } from '../src/cli.js';
+import { welcomeUser } from './cli.js';
 
+export const brainEven = (name) => {
+  welcomeUser(name);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-export const brainEven = ( name ) => {
-    welcomeUser(name);
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === 'yes' && randomNumber % 2 === 0) {
+      console.log('Correct!');
+    } else if (answer === 'no' && randomNumber % 2 >= 1) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+    }
+  }
 
-    for (let i = 0; i < 3; i++) {
-        let randomNumber = Math.floor(Math.random() * 100);
-        console.log(`Question: ${randomNumber}`);
-        let answer = readlineSync.question('Your answer: ');
-        if (answer == 'yes' && randomNumber % 2 === 0) {
-            console.log('Correct!');
-        } else if (answer == 'no' && randomNumber % 2 >= 1) {
-            console.log('Correct!');
-        } else {
-            return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-        };   
-    };
+  return console.log(`Congratulations, ${name}!`);
+};
 
-    console.log(`Congratulations, ${name}!`);
-
-    };
-
-    export default brainEven;
+export default brainEven;
