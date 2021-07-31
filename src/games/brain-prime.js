813e38f24da3d397fc21b60449c1225112e044e3
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-07-31 16:08:14 +0600

    review 2-3

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,18 +1,17 @@
 import engine from '../index.js';
-import randomNumber from './random-number.js';
 
 const gameInfo = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-export const calculateValues = () => {
-  const num = randomNumber();
+export const brainPrimeLogic = () => {
+  const num = Math.floor(Math.random() * (100 - 1) + 1);
   let count = 0;
   for (let j = 0; j < num; j += 1) {
-    if (num % j === 0 && num > 1) {
+    if (num % j === 0) {
       count += 1;
     }
   }
   const isPrime = count > 1 ? 'no' : 'yes';
+
   return [num, isPrime];
 };
 
-export const brainPrime = () => engine(calculateValues, gameInfo);
+export const brainPrime = () => engine(brainPrimeLogic, gameInfo);
