e55d2cb0013cc7ce190687bb5608e22b08a1a5f6
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-08-03 23:40:27 +0600

    ref 3-2

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -6,7 +6,7 @@ const gameInfo = 'Find the greatest common divisor of given numbers.';
 const calculateDevisor = (first, second) => {
   let devisor = 1;
   const calculationRange = first > second ? second : first;
-  for (let i = 1; i < calculationRange; i += 1) {
+  for (let i = 1; i <= calculationRange; i += 1) {
     if (first % i === 0 && second % i === 0) {
       devisor = i;
     }
